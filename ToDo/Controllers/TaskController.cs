5080b48ed9588c8c528f0791945b4c4452ee7793
Elmir <mollymaloun@gmail.com>
2023-06-07 21:37:12 +0300

    Initial commit

---
+++ ToDo/Controllers/TaskController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ToDo.Models;
+
+namespace ToDo.Controllers
+{
+    public class TaskController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ ToDoDomain/Entity/TaskEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Domain.Enum;
+
+namespace ToDo.Domain.Entity
+{
+    public class TaskEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Priority Priority { get; set; }
+    }
+}
